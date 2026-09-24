3a264aeedf3a9a61febce662418ca6ae0e8b89f3
chris <chrisdieria@gmail.com>
2026-09-24 18:28:00 -0400

    commit 3

--- backend/save_guests/admin.py
+++ backend/save_guests/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from .models import Attendance
-
-admin.site.register(Attendance)
 @admin.register(Attendance)
 class AttendanceAdmin(admin.ModelAdmin):
     list_display = ("name", "attendance")
