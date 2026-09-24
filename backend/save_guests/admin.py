24b3bfd20c122d1d4d4e7509c1742ddec8858475
chris <chrisdieria@gmail.com>
2026-09-24 18:24:33 -0400

    commit

--- backend/save_guests/admin.py
+++ backend/save_guests/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Attendance
-
-admin.site.register(Attendance)
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ("name", "attendance")
 
 # Register your models here.
--- backend/save_guests/models.py
+++ backend/save_guests/models.py
@@ -5,6 +5,3 @@ from django.db import models
 class Attendance(models.Model):
     name = models.CharField(max_length=200)
     attendance_confirmation = models.CharField(max_length=200)
-
-    def __str__(self):
-        return f"{self.name} - {self.attendance}"
